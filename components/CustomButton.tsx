692a5e76296546128d06b8e4ec629ccb6a897908
AungChanNyeinC-137 <aungchannyein29699@gmail.com>
2025-07-06 16:20:41 +0630

    Implemented custombutton
    onPress
    title
    style
    textStyle
    leftIcon
    isLoading

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { CustomButtonProps } from '@/type';
+import cn from 'clsx';
+import React from 'react';
+import { ActivityIndicator, Text, TouchableOpacity, View } from 'react-native';
 
-const CustomButton = () => {
+
+const CustomButton = ({
+  onPress,
+  title = 'Click me',
+  style,
+  textStyle,
+  leftIcon,
+  isLoading = false
+}: CustomButtonProps) => { 
   return (
-    <View>
-      <Text>CustomButton</Text>
-    </View>
+    <TouchableOpacity 
+    className={cn('custom-btn',style)}
+    onPress={onPress} 
+    >
+      {leftIcon}
+      <View className='flex-center flex-row'>{isLoading?(
+        <ActivityIndicator size='small' color='white'/>
+      ): (
+        <Text className={cn('text-white-100 paragraph-semibold',textStyle)}>
+          {title}
+        </Text>
+      )}</View>
+      
+    </TouchableOpacity> 
   )
 }
 
